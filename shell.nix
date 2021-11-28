1a92cf1833cd1b4a55bd2c4837d9995dbad0789c
Gautier DI FOLCO <gautier.difolco@gmail.com>
2021-11-28 12:55:09 +0100

    Fix nixpkgs version in shell.nix (#42)

--- shell.nix
+++ shell.nix
@@ -1,5 +1,11 @@
-let pkgs = import (fetchTarball path) {};
-    path = https://github.com/NixOS/nixpkgs/archive/master.tar.gz;
+let
+  githubTarball = owner: repo: rev:
+    builtins.fetchTarball { url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz"; };
+
+  nixpkgsSrc = commit:
+    githubTarball "NixOS" "nixpkgs" commit;
+
+  pkgs = import (nixpkgsSrc "5fd4f796b4210d691b1f89e1f29043d635cd20e0") { };
 in with pkgs;
   mkShell {
     LOCALE_ARCHIVE_2_27 = "${glibcLocales}/lib/locale/locale-archive";
